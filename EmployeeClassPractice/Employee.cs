6a5c44aeff264a951210149ff03d47b1e96111b3
Scott <70402467+SWilli93@users.noreply.github.com>
2024-05-06 13:37:25 -0400

    Initial commit

---
+++ EmployeeClassPractice/Employee.cs
@@ -0,0 +1,58 @@
+namespace EmployeeClassPractice;
+
+public class Employee
+{
+    public string firstName;
+    public string lastName;
+    public string email;
+
+    public int numberOfHoursWorked;
+    public double wage;
+    public double hourlyRate;
+
+    public DateTime birthDay;
+
+    private const int minimalHoursWorkedUnit = 1;
+    
+    public Employee(string first, string last, string em, DateTime bd) : this(first, last, em, bd, 0) {}
+
+    public Employee(string first, string last, string em, DateTime bd, double rate)
+    {
+        firstName = first;
+        lastName = last;
+        email = em;
+        birthDay = bd;
+        hourlyRate = rate;
+    }
+
+    public void PerformWork()
+    {
+        PerformWork(minimalHoursWorkedUnit);
+    }
+
+    public void PerformWork(int numberOfHours)
+    {
+        numberOfHoursWorked += numberOfHours;
+        
+        Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHours} hour(s)!");
+    }
+
+    public double RecieveWage(bool resetHours = true)
+    {
+        wage = numberOfHoursWorked * hourlyRate;
+        
+        Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work.");
+
+        if (resetHours)
+        {
+            numberOfHoursWorked = 0;
+        }
+
+        return wage;
+    }
+
+    public void DisplayEmployeeDetails()
+    {
+        Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail: \t{email}\nBirthday: \t{birthDay.ToShortDateString()}\n");
+    }
+}
---
+++ EmployeeClassPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EmployeeClassPractice;
+
+Console.WriteLine("Creating an Employee");
+Console.WriteLine("--------------------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "example@example.com", new DateTime(1979,1,16),25);
+
+bethany.DisplayEmployeeDetails();
+
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork();
+bethany.PerformWork(5);
+bethany.PerformWork();
+
+double recievedWageBethany = bethany.RecieveWage(true);
+Console.WriteLine($"Wage paid (message from Program: {recievedWageBethany}");
